e2e99d83cd1f94831dfbbf38452cc2c785be6564
Guillem Bonet <guillem.bonet@gmail.com>
2023-02-08 22:28:59 +0100

    small fixes
    Signed-off-by: Guillem Bonet <guillem.bonet@gmail.com>

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"crypto/rand"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -23,32 +22,32 @@ func main() {
 		sizeMBString := r.URL.Query().Get("size_mb")
 		sizeMB, err := strconv.Atoi(sizeMBString)
 		if err != nil {
-			http.Error(w, "Invalid size_mb", http.StatusBadRequest)
+			http.Error(w, "invalid size_mb", http.StatusBadRequest)
 			return
 		}
 		MBpsString := r.URL.Query().Get("mbs")
 		MBps, err := strconv.Atoi(MBpsString)
 		if err != nil {
-			http.Error(w, "Invalid mbs", http.StatusBadRequest)
+			http.Error(w, "invalid mbs", http.StatusBadRequest)
 			return
 		}
 		log.Printf("serving %d MB file...\n", sizeMB)
-		defer log.Printf("finished serving %d MB file\n", sizeMB)
 		randomBytes := make([]byte, sizeMB*oneMB)
 		_, err = rand.Read(randomBytes)
 		if err != nil {
-			http.Error(w, "Error generating random data", http.StatusInternalServerError)
+			http.Error(w, "error generating random data", http.StatusInternalServerError)
 			return
 		}
 
-		tick := time.Tick(100 * time.Millisecond)
+		tick := time.Tick(10 * time.Millisecond)
 		bytesWritten := 0
 		for {
 			<-tick
 			if bytesWritten >= sizeMB*oneMB {
+				log.Printf("finished serving %d MB file successfully.\n", sizeMB)
 				return
 			}
-			n := MBps * oneMB / 10
+			n := MBps * oneMB / 100
 			if n > sizeMB*oneMB-bytesWritten {
 				n = sizeMB*oneMB - bytesWritten
 			}
@@ -62,10 +61,10 @@ func main() {
 	})
 
 	if FlagAddress == nil {
-		panic("FlagAddress is nil")
+		panic("flagAddress is nil")
 	}
 
-	fmt.Printf("Listening on %s...\n", *FlagAddress)
+	log.Printf("listening on %s...\n", *FlagAddress)
 	err := http.ListenAndServe(*FlagAddress, nil)
 	if err != nil {
 		panic(err)
